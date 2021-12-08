91bbe7457dccd3b4f7201dbc109af735029ba1ab
Martin Bove <martin@fanalitica.com>
2021-12-08 19:38:11 -0300

    Initial Commit

---
+++ WheatherTest/WheatherTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherTestApp.swift
+//  WheatherTest
+//
+//  Created by Resonant Sports on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WheatherTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
