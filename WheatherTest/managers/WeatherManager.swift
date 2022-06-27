b58a2b3ceaf814e77cf009443e332ab8bce65e3b
Martin Bove <mbove77@gmail.com>
2022-06-27 19:26:19 -0300

    Update WeatherManager.swift

--- WheatherTest/managers/WeatherManager.swift
+++ WheatherTest/managers/WeatherManager.swift
@@ -11,7 +11,7 @@ import CoreLocation
 class WeatherManager {
     func getCurrentWeather(latitude:CLLocationDegrees, longitude:CLLocationDegrees) async throws -> ResponseBody {
         
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=59c34ff494a5582577ccd4a79b109c32&units=metric") else { fatalError("Missing URL.")}
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=YOUR_APPID_HERE&units=metric") else { fatalError("Missing URL.")}
         
         let urlRequest = URLRequest(url: url)
         
